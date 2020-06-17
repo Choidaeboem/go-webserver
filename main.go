3975317e2f575f382542ade6d726a87f9eabeadb
Choidaeboem <chleoqja0125@nate.com>
2020-06-17 23:27:54 +0900

    JSON control

--- main.go
+++ main.go
@@ -1,28 +1,59 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 )
+
+type User struct {
+	FirstName	string	`json:"first_name"`
+	LastName	string	`json:"last_name"`
+	Email		string	`json:"email"`
+	CreatedAt time.Time	`json:"created_at"`
+}
 
 type fooHandler struct {
 
 }
 
 func (f *fooHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello Foo")
+	user := new(User)
+	err := json.NewDecoder(r.Body).Decode(user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, "Bad Request: ", err)
+		return
+	}
+
+	user.CreatedAt = time.Now()
+	data, _ := json.Marshal(user)
+	
+	w.Header().Add("content-type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprint(w, string(data))
+}
+
+func barHanlder (w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+	if name == "" {
+		name = "World"
+	}
+
+	fmt.Fprintf(w, "Hello %s!", name)
 }
 
 func main() {
-	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello World")
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello World Go")
 	})
 
-	http.HandleFunc("/bar", func (w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello bar!")
-	})
+	mux.HandleFunc("/bar", barHanlder)
 
-	http.Handle("/foo", &fooHandler{})
+	mux.Handle("/foo", &fooHandler{})
 
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", mux)
 }
